82767fd9ce69c983b85aca288a4feedd052f72c3
mihai <saftacatalinmihai@gmail.com>
2017-03-27 17:45:02 +0300

    Added quil graphic lib dependency

--- code/frontend/src/viz/core.cljs
+++ code/frontend/src/viz/core.cljs
@@ -2,7 +2,9 @@
   (:use-macros
     [dommy.macros :only [node sel sel1]])
   (:require
-    [dommy.core :as dommy]))
+    [dommy.core :as dommy]
+    [quil.core :as q :include-macros true]
+    [quil.middleware :as m :include-macros true]))
 
 (.log js/console "Hey Seymore sup?!")
 
@@ -35,4 +37,47 @@
 
 (dommy/listen! (sel1 :#new-actor) :keyup new_actor!)
 
+(def min-r 10)
+(def max-r 20)
 
+(defn setup []
+  ; initial state
+  {:x 0 :y 0 :r min-r, :movement :dec})
+
+(defn draw [state]
+  (q/background 255)
+  (q/ellipse (:x state) (:y state) (:r state) (:r state)))
+
+(defn update_sketch [state]
+  ; increase radius of the circle by 1 on each frame
+;  (.log js/console (:movement state))
+  (update-in
+    (cond
+      ( = max-r (:r state)) (update-in state [:movement] :dec)
+      ( = min-r (:r state)) (update-in state [:movement] :inc)
+      :else state)
+;    [:r] inc))
+    [:r] (cond
+       (= :dec (:movement state)) dec
+       (= :inc (:movement state)) inc
+           :else dec )))
+
+; decrease radius by 1 but keeping it not less than min-r
+(defn shrink [r]
+  (max min-r (dec r)))
+
+(defn mouse-moved [state event]
+  (-> state
+      ; set circle position to mouse position
+      (assoc :x (:x event) :y (:y event))
+      ; decrease radius
+      (update-in [:r] shrink)))
+
+(q/defsketch example
+  :host "canvas-id"
+  :size [400 400]
+  :setup setup
+  :draw draw
+  :update update_sketch
+  :mouse-moved mouse-moved
+  :middleware [m/fun-mode])
