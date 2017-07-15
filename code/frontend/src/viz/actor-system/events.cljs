e790452ce9d270821273919d3a0eb84664d2e6c7
Mihai Safta <saftacatalinmihai@gmail.com>
2017-07-15 21:50:31 +0300

    Changed events specs to use multi-spec

--- code/frontend/src/viz/actor-system/events.cljs
+++ code/frontend/src/viz/actor-system/events.cljs
@@ -2,12 +2,17 @@
   (:require [cljs.spec :as s]
             [viz.actor-system.specs :as actor-system]))
 
-(s/def ::actor-spawned (s/keys :req-un [::actor-system/actor]))
-(s/def ::message-sent (s/keys :req-un [::actor-system/message]))
-(s/def ::message-received (s/keys :req-un [::actor-system/message]))
+(s/def :event/type keyword?)
+
+(defmulti event-type :event/type)
+(defmethod event-type :event/actor-spawned [_] (s/keys :req [:event/type ::actor-system/process]))
+(defmethod event-type :event/message-sent [_] (s/keys :req [:event/type ::actor-system/message]))
+(defmethod event-type :event/message-received [_] (s/keys :req [:event/type ::actor-system/message]))
+
+(s/def :event/event (s/multi-spec event-type :event/type))
 
 (defn -test []
-  (assert (s/valid? ::actor-spawned {:actor {:pid "<some-pid>" :type "SomeActorType"}}))
-  (assert (s/valid? ::message-sent {:message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
-  (assert (s/valid? ::message-received {:message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
+  (assert (s/valid? :event/event {:event/type :event/actor-spawned ::actor-system/process {:pid "<some-pid>" :type "SomeActorType"}}))
+  (assert (s/valid? :event/event {:event/type :event/message-sent ::actor-system/message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
+  (assert (s/valid? :event/event {:event/type :event/message-received ::actor-system/message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
   )
--- code/frontend/src/viz/actor-system/specs.cljs
+++ code/frontend/src/viz/actor-system/specs.cljs
@@ -7,9 +7,9 @@
 (s/def ::to-pid ::pid)
 (s/def ::type string?)
 (s/def ::content string?)
-(s/def ::actor (s/keys :req-un [::pid ::type]))
+(s/def ::process (s/keys :req-un [::pid ::type]))
 (s/def ::message (s/keys :req-un [::from-pid ::to-pid ::content]))
 
 (defn -test []
-  (assert (s/valid? ::actor {:pid "<asd>" :type "Actor2"}))
+  (assert (s/valid? ::process {:pid "<asd>" :type "Actor2"}))
   (assert (s/valid? ::message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"})))
