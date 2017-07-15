626db85b26cc53a66ee52c012ea7ebeee3f7a8bf
Mihai Safta <saftacatalinmihai@gmail.com>
2017-07-15 21:22:48 +0300

    Added message to specs and added message-sent and message-received events.

--- code/frontend/src/viz/actor-system/events.cljs
+++ code/frontend/src/viz/actor-system/events.cljs
@@ -2,9 +2,12 @@
   (:require [cljs.spec :as s]
             [viz.actor-system.specs :as actor-system]))
 
-(s/def ::spawn (s/keys :req-un [::actor-system/actor]))
-(s/def ::message (s/keys :req-un [::actor-system/from-pid ::actor-system/to-pid ::actor-system/message]))
+(s/def ::actor-spawned (s/keys :req-un [::actor-system/actor]))
+(s/def ::message-sent (s/keys :req-un [::actor-system/message]))
+(s/def ::message-received (s/keys :req-un [::actor-system/message]))
 
 (defn -test []
-  (assert
-    (s/valid? ::spawn {:actor {:pid "<some-pid>" :type "SomeActorType"}})))
+  (assert (s/valid? ::actor-spawned {:actor {:pid "<some-pid>" :type "SomeActorType"}}))
+  (assert (s/valid? ::message-sent {:message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
+  (assert (s/valid? ::message-received {:message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
+  )
--- code/frontend/src/viz/actor-system/specs.cljs
+++ code/frontend/src/viz/actor-system/specs.cljs
@@ -6,8 +6,10 @@
 (s/def ::from-pid ::pid)
 (s/def ::to-pid ::pid)
 (s/def ::type string?)
-(s/def ::message string?)
+(s/def ::content string?)
 (s/def ::actor (s/keys :req-un [::pid ::type]))
+(s/def ::message (s/keys :req-un [::from-pid ::to-pid ::content]))
 
 (defn -test []
-  (assert (s/valid? ::actor {:pid "<asd>" :type "Actor2"})))
+  (assert (s/valid? ::actor {:pid "<asd>" :type "Actor2"}))
+  (assert (s/valid? ::message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"})))
