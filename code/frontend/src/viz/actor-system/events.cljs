3ee8c9884635963775ae445ca12c7d831c1301a4
Mihai Safta <saftacatalinmihai@gmail.com>
2017-07-16 00:33:51 +0300

    Added a protocol for handling actor-system events.
    Added an implementation for keeping the events in a in-memory store.

--- code/frontend/src/viz/actor-system/events.cljs
+++ code/frontend/src/viz/actor-system/events.cljs
@@ -11,7 +11,26 @@
 
 (s/def :event/event (s/multi-spec event-type :event/type))
 
+(defprotocol EventHandler
+  "Handles an actor system event"
+  (handle [this ev] "Handle the event"))
+
+(deftype Store [event-list]
+  EventHandler
+  (handle [this ev] (swap! event-list conj ev)))
+
+(defn store
+  ([] (Store. (atom [])))
+  ([ev-list] (Store. (atom ev-list))))
+
 (defn -test []
+  (let [s (store)
+        e {:event/type :event/actor-spawned ::actor-system/process {:pid "<some-pid>" :type "SomeActorType"}}]
+    (js/console.log (pr-str (handle s e)))
+    (js/console.log (pr-str (handle s e)))
+    (js/console.log (pr-str (handle s e)))
+    (js/console.log (pr-str @(.-event-list s))))
+
   (assert (s/valid? :event/event {:event/type :event/actor-spawned ::actor-system/process {:pid "<some-pid>" :type "SomeActorType"}}))
   (assert (s/valid? :event/event {:event/type :event/message-sent ::actor-system/message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
   (assert (s/valid? :event/event {:event/type :event/message-received ::actor-system/message {:from-pid "<1.2.1>" :to-pid "<1.2.2>" :content "{1,2}"}}))
